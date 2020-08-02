93f7fdada362e174367865c1a409a45ec5543420
Viktoria Kolesnik <67219664+VictoriaKolesnyk@users.noreply.github.com>
2020-08-02 13:38:54 +0300

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -1,15 +1,15 @@
 def arithmetic(arg1, arg2, arg3):
     if arg3 == '+':
-        return print(arg1 + arg2)
+        return arg1 + arg2
     elif arg3 == '-':
-        return print(arg1 - arg2)
+        return arg1 - arg2
     elif arg3 == '*':
-        return print (arg1 * arg2)
+        return arg1 * arg2
     elif arg3 == '/':
-        return print(arg1 / arg2)
+        return arg1 / arg2
     else:
-        return print( "Неизвестная операция")
+        return "Неизвестная операция"
 arg1 = int(input('введите первое число:  '))
 arg2 = int(input('введите второе число:  '))
 arg3 = input('желаемая операция: ')
-arithmetic(arg1, arg2, arg3)
+print(arithmetic(arg1, arg2, arg3))
